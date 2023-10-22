8c84fc46251b66f48b5a7168e91920db237c66c1
Pham Anh <contact.vananh0505@gmail.com>
2023-10-22 22:08:35 +0700

    fix cors

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import connectDB from "./configs/connectDB.js";
 import connectDB_01 from "./configs/connectDB_01.js";
 import accountRoutes from "./routes/accountRoutes.js";
+import employeeRoutes from "./routes/employeeRoutes.js";
 dotenv.config();
 connectDB_01();
 
@@ -15,11 +16,37 @@ app.use(express.json());
 // Parse URL-encoded form data with extended option
 app.use(express.urlencoded({ extended: true }));
 
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4000');
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("API running");
 });
 
 app.use("/api/account", accountRoutes);
+app.use("/api/employee", employeeRoutes);
 
 const PORT = process.env.PORT || 5000;
 
